ff45ae0e647c1c7b23c8095b3be3fd11426649bb
Ethan Williams <ethan.r.williams@live.com>
2015-05-22 22:42:42 +1000

    add execute function to squel

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ var squel = require('squel');
 squel.adapter = function(){};
 squel.cls.QueryBuilder.prototype.execute = function(cb){
   squel.adapter(this.toString(), cb);
+  return this;
 };
 module.exports = squel;
